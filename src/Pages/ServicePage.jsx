c287c5dddad36bf5e858c4804025fec6aab2bc2d
Michael Li <limichael391@gmail.com>
2025-10-29 20:57:01 +1100

    changed images and added services in service page

--- src/Pages/ServicePage.jsx
+++ src/Pages/ServicePage.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+// Import images
+import ceramicCoatingImg from "@/images/service_img/ceramic-coating.webp";
+import carWawshImg from "@/images/service_img/carwash.webp";
+import engineBayWash from "@/images/service_img/engine_bay_wash.webp";
+import shampoo_carpet from "@/images/service_img/shampoo_carpet.webp";
+import headlight_restoration from "@/images/service_img/headlight_restoration.webp";
+import car_dent_removal from "@/images/service_img/car_dent_removal.webp";
+import paint_correction from "@/images/service_img/paint_correction.webp"
+
+
+
 const ServicePage = () => {
   const services = [
     {
@@ -9,7 +20,7 @@ const ServicePage = () => {
       description:
         "Our exterior detailing service brings your vehicle’s shine back to life. We remove dirt, grime, and contaminants with a thorough wash, followed by hand waxing or machine polishing to protect your paint and enhance gloss. It’s more than a wash—it’s a full rejuvenation of your car’s outer appearance.",
       imageUrl:
-        "https://dpkoly04s20lw.cloudfront.net/resources/services/Exterior%20Car%20Detailing,%20Polishing%20_%20Cleaning-min.jpg",
+        carWawshImg,
       imagePosition: "left",
     },
     {
@@ -18,7 +29,7 @@ const ServicePage = () => {
       description:
         "We deep clean every corner of your vehicle’s interior, from seats and carpets to dashboards and vents. Whether it's leather, fabric, or plastic, we restore, sanitise, and refresh surfaces to a like-new condition. Enjoy that fresh car feel every time you step inside.",
       imageUrl:
-        "https://surfnshine.com/wp-content/uploads/2023/10/interior-detailing.jpg",
+        "https://www.thedetailingexperts.com.au/wp-content/uploads/2022/04/Mass-blog-header-template-01-2.png",
       imagePosition: "right",
     },
     {
@@ -27,45 +38,58 @@ const ServicePage = () => {
       description:
         "Restore your car’s finish with our professional paint correction. We carefully remove swirl marks, scratches, and oxidation to bring out a flawless, high-gloss shine. Perfect for vehicles needing that extra level of care.",
       imageUrl:
-        "https://cdn.shopify.com/s/files/1/0797/2968/8869/files/a5.jpg?v=1692785397&width=1620&height=1080",
+      paint_correction,
+        //"https://cdn.shopify.com/s/files/1/0797/2968/8869/files/a5.jpg?v=1692785397&width=1620&height=1080",
       imagePosition: "left",
     },
     {
       id: 4,
-      title: "Minor Dent removal ",
+      title: "Ceramic Coating",
       description:
-        "Got a dent or ding? Our minor dent removal service targets those small imperfections without needing a full panel respray. Using precision techniques, we restore your vehicle’s smooth finish quickly and affordably.",
+        "Give your paint long-lasting protection and deep gloss with our professional ceramic coating. This advanced layer bonds to your car’s surface, repelling water, dirt, and UV rays while enhancing shine and making maintenance effortless. Keep your vehicle looking new for years with premium ceramic protection.",
       imageUrl:
-        "https://chicagoautohaus.com/wp-content/uploads/2023/10/dent-removal-on-car-1024x574.png",
+        ceramicCoatingImg,
       imagePosition: "right",
     },
     {
       id: 5,
-      title: "Headlight restoration",
-      description:
-        "Cloudy, yellow headlights can reduce both visibility and the look of your vehicle. Our headlight restoration service clears up foggy lenses and protects them, improving nighttime driving safety and the overall look of your car.",
-      imageUrl:
-        "https://t4.ftcdn.net/jpg/10/82/54/77/360_F_1082547727_c5o8rAUG7z9fPxfOuch8iAroelPFJebt.jpg",
-      imagePosition: "left",
-    },
-    {
-      id: 6,
-      title: "Engine bay wash ",
-      description:
-        "A clean engine bay not only looks impressive but also helps with maintenance and spotting issues early. We carefully degrease and rinse your engine compartment, taking care not to damage sensitive components.",
-      imageUrl:
-        "https://www.3dcarcaremiami.com/cdn/shop/articles/Engine_pic_blog_1140x.webp?v=1674779209",
-      imagePosition: "right",
-    },
-    {
-      id: 7,
       title: "Odour Removal",
       description:
         "Say goodbye to stubborn smells with our professional odour removal service. Whether it’s smoke, pet, food, or mildew, we target and neutralize the source—not just mask it. Your vehicle will be left smelling clean, fresh, and truly revitalized.",
       imageUrl:
-        "https://www.thedetailingexperts.com.au/wp-content/uploads/2022/04/Mass-blog-header-template-01-2.png",
+        shampoo_carpet,
       imagePosition: "left",
     },
+    {
+      id: 6,
+      title: "Headlight restoration",
+      description:
+        "Cloudy, yellow headlights can reduce both visibility and the look of your vehicle. Our headlight restoration service clears up foggy lenses and protects them, improving nighttime driving safety and the overall look of your car.",
+      imageUrl:
+        headlight_restoration,
+      imagePosition: "right",
+    },
+    {
+      id: 7,
+      title: "Engine bay wash ",
+      description:
+        "A clean engine bay not only looks impressive but also helps with maintenance and spotting issues early. We carefully degrease and rinse your engine compartment, taking care not to damage sensitive components.",
+      imageUrl:
+        engineBayWash,
+      imagePosition: "left",
+    },
+    {
+      id: 8,
+      title: "Minor Dent removal ",
+      description:
+        "Got a dent or ding? Our minor dent removal service targets those small imperfections without needing a full panel respray. Using precision techniques, we restore your vehicle’s smooth finish quickly and affordably.",
+      imageUrl:
+        car_dent_removal,
+      imagePosition: "right",
+    },
+    
+    
+    
   ];
 
   const fadeInUp = {
